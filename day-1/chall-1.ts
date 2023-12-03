5218e38d5267bd7ef075e066ace92edcc779cd84
Maximilian Kemkes <109769614+MaxKemkes@users.noreply.github.com>
2023-12-03 19:51:37 +0100

    day1

---
+++ day-1/chall-1.ts
@@ -0,0 +1,41 @@
+import {readFileSync} from "fs"
+
+const lines = readFileSync("day-1/lines.txt", "utf-8")
+	.split(/\r\n/)
+	.map((e) => (e.replace(/([A-Za-z])/g, "")));
+
+// console.log(lines)
+
+
+const nums: number[] = lines.map(e => {
+    let left: number | null = null;
+    let right: number | null = null
+    const len = e.length
+    for (let i = 0; i < len; i++){
+        if (left && right){
+            return left + right
+        }
+
+        if (!left){
+			//@ts-ignore
+			if (!isNaN(e[i])) left = e[i];
+		}
+        
+        if (!right) {
+            // console.log(i, e[-i])
+            //@ts-ignore
+            if (!isNaN(e[len-1 -i])) right = e[len - 1-i];
+        }
+    }
+
+    //@ts-ignore
+    return left + right
+})
+
+console.log(nums)
+
+const sum = nums.reduce((acc, curr) => {
+    return acc += curr * 1
+}, 0)
+
+console.log(sum)
---
+++ day-1/chall-2.ts
@@ -0,0 +1,107 @@
+import {readFileSync} from "fs"
+
+const numDict = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+    zero:"0"
+} as const
+
+const replaceFirst = (str: string, subStr: keyof typeof numDict, replace: typeof numDict[keyof typeof numDict]) => {
+    return str.replace(subStr, replace)
+}
+
+const replaceLast = (str: string, subStr: keyof typeof numDict, replace: typeof numDict[keyof typeof numDict]) => {
+	let list = str.split(subStr)
+    var lastList = list.pop()
+
+    return list.join(subStr) + replace + lastList
+}
+
+const lines = readFileSync("day-1/lines.txt", "utf-8")
+	.split(/\r\n/)
+	.map((e) => {
+		let str = e;
+        let firstIndex: [number, keyof typeof numDict, typeof numDict[keyof typeof numDict]]  = [9999999, null, null];
+        let lastIndex: [number, keyof typeof numDict, typeof numDict[keyof typeof numDict]] = [-1, null, null];
+
+		for (const [key, value] of Object.entries(numDict)) {
+            if (str.indexOf(key) < firstIndex[0] && str.indexOf(key) !== -1){
+                firstIndex= [str.indexOf(key), key as keyof typeof numDict, value]
+            }
+		}
+        
+        for (const [key, value] of Object.entries(numDict)){
+            if (
+				str.lastIndexOf(key) > lastIndex[0] &&
+				str.indexOf(key) !== -1
+			) {
+				lastIndex = [
+					str.lastIndexOf(key),
+					key as keyof typeof numDict,
+					value,
+				];
+			}
+        }
+        if (str === "twofivesix34nine") {
+            console.log(str)
+            console.log(firstIndex)
+            console.log(lastIndex)
+        }
+        
+        if (str.search(/\d/) > firstIndex[0]){
+            str = replaceFirst(str, firstIndex[1], firstIndex[2])
+        }
+        str = replaceLast(str, lastIndex[1], lastIndex[2])
+
+          if (str === "twofivesix34nine") {
+				console.log(str);
+				// console.log(firstIndex);
+				// console.log(lastIndex);
+        }
+
+        return str;
+	})
+	.map((e) => e.replace(/([A-Za-z])/g, ""));
+
+console.log(lines)
+
+
+const nums: number[] = lines.map(e => {
+    let left: number | null = null;
+    let right: number | null = null
+    const len = e.length
+    for (let i = 0; i < len; i++){
+        if (left && right){
+            return left + right
+        }
+
+        if (!left){
+			//@ts-ignore
+			if (!isNaN(e[i])) left = e[i];
+		}
+        
+        if (!right) {
+            // console.log(i, e[-i])
+            //@ts-ignore
+            if (!isNaN(e[len-1 -i])) right = e[len - 1-i];
+        }
+    }
+
+    //@ts-ignore
+    return left + right
+})
+
+console.log(nums)
+
+const sum = nums.reduce((acc, curr) => {
+    return acc += curr * 1
+}, 0)
+
+console.log(sum)
