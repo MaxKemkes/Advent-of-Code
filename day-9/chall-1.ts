368a99fcbd75282ab74d4133429d0b16bc50a367
MaxKemkes <109769614+MaxKemkes@users.noreply.github.com>
2023-12-09 10:29:27 +0100

    day 9

---
+++ day-9/chall-1.ts
@@ -0,0 +1,42 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+var __dirname = path.dirname(fileURLToPath(import.meta.url));
+const lines = readFileSync(
+	path.resolve(__dirname, "quizz.txt"),
+	"utf-8"
+).split(/\r\n/).map(e => e.split(" ").map(n => Number(n)));
+
+console.log(lines)
+
+function returnNext(arr: number[]){
+    const slope: number[] = []
+    let allZeros = true
+
+    for (let i  = 1; i < arr.length; ++i){
+        const diff = arr[i] - arr[i-1]
+        allZeros &&= (diff === 0)
+        slope.push(arr[i] - arr[i-1])
+    }
+
+    if (allZeros) return arr[0]
+
+    console.log("Slope", slope)
+    return arr[arr.length - 1] + returnNext(slope)
+}
+
+const results:number[] = []
+lines.forEach(e => {
+    console.log(e)
+
+    const result = returnNext(e)
+    results.push(result)
+    console.log("Res", result)
+})
+
+const sum = results.reduce((acc, curr) => {
+    return acc + curr
+}, 0)
+
+console.log("Sum", sum)
